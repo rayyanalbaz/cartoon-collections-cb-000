4ccb5a2110968079b0fe2da047d68d34f4fb1abb
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-12 23:11:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  i = 0
+  while i < array.length
+
+  end
 end
 
 def summon_captain_planet# code an argument here
