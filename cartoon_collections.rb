aabf953f447bf2d3f0436075458c47811231f7bb
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-12 23:15:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   i = 0
   while i < array.length
-    puts "#{i+1} array[i]"
+    puts "#{i+1}." array[i]
   i=i+1
   end
 end
