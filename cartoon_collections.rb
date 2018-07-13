02d496ede96f42211591ecc2f000eb1c1887496c
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-13 00:06:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.each { |e| if e.lenght < 4 then array.delete(e) end}
+  array.each { |e| if e.length < 4 then array.delete(e) end}
   # Your code here
   array.empty?
 end
