4c918793ff65de7be168bbf368fd4695a1ea9700
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-13 00:29:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,10 @@ end
 
 def long_planeteer_calls(array)# code an argument here
 array.each { |e| if e.length < 4 then array.delete(e)end}
-  array.empty?
+if array.empty?
+  true 
+else
+  false
 end
 
 def find_the_cheese# code an argument here
