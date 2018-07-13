feb6fbeb0adb679884bfed4ee7edcbd400e4b169
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-13 00:22:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.each { |e| if e.length > 3 then true end}
+  array.each { |e|  e.length > 3 then true}
   # Your code here
 end
 
