c30d2668992d041b74e665b025ba1430bf2f367b
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-13 00:22:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ end
 def long_planeteer_calls(array)# code an argument here
   array.each { |e| if e.length > 3 then true end}
   # Your code here
+end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
