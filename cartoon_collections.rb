f1dfc3de98073cff627c87f964f8992274b26bbe
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-13 00:31:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-array.each { |e| if e.length < 4 then array.delete(e)end}
-return array 
+array.each { |e| if e.length < 4 then array.delete(e)end array.empty?}
+
 end
 
 def find_the_cheese# code an argument here
