133e9f8878ef3959782bf569edd8b66ddfcaad9e
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-13 00:11:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 def long_planeteer_calls(array)# code an argument here
   array.each { |e| if e.length < 4 then array.delete(e) end}
   # Your code here
-  if array.size => 0
+  if array.size > 0
     return true
   else
     return false
